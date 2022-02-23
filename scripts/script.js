f563ef15b2e581022dd3edbd7224e89075efbf7f
gcerv93 <gcerv93@protonmail.com>
2022-02-23 17:35:32 -0600

    set up display of board array

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,23 @@
+const playerFactory = (name, symbol) => {
+  return { name, symbol };
+};
+
 const gameBoard = (() => {
-  board = ['', '', '', '', '', '', '', '']
-  return { board }
+  const board = ['', '', '', '', '', '', '', '', ''];
+  return { board };
 })();
 
-const playerFactory = (name, symbol) => {
-  return { name, symbol }
-};
+const displayController = (() => {
+  const updateDisplay = (board) => {
+    board.forEach((cell, index) => {
+      const displayCell = document.querySelector(`[data-index="${index}"]`);
+      displayCell.textContent = cell;
+    });
+  };
+
+  return { updateDisplay };
+})();
+
+// temp for checking display
+// const board = gameBoard.board;
+// displayController.updateDisplay(board);
