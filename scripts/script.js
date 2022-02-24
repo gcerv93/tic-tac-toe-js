d0c0eba6ae65e1510e4c4ba6d1d697c7bec0b90e
gcerv93 <gcerv93@protonmail.com>
2022-02-24 10:29:15 -0600

    add working click handler and display

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-const playerFactory = (name, symbol) => {
-  return { name, symbol };
+const playerFactory = (symbol) => {
+  return { symbol };
 };
 
 const gameBoard = (() => {
-  const board = [null, null, null, null, null, null, null, null, null];
+  const board = ['', '', '', '', '', '', '', '', ''];
 
   const updateBoard = (cell, symbol) => {
     board[cell] = symbol;
@@ -13,6 +13,8 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
+  const cells = document.querySelectorAll('[data-index]');
+
   const updateDisplay = (board) => {
     board.forEach((cell, index) => {
       const displayCell = document.querySelector(`[data-index="${index}"]`);
@@ -20,6 +22,27 @@ const displayController = (() => {
     });
   };
 
-  return { updateDisplay };
+  return { updateDisplay, cells };
 })();
 
+const gameController = (() => {
+  const playerone = playerFactory('X');
+  const playertwo = playerFactory('O');
+  let currentPlayer = playerone;
+
+  const _changeCurrPlayer = () => {
+    currentPlayer = currentPlayer === playerone ? playertwo : playerone
+  }
+
+  const _clickHandler = (e) => {
+    if (e.target.textContent === '') {
+      gameBoard.updateBoard(e.target.dataset.index, currentPlayer.symbol);
+      displayController.updateDisplay(gameBoard.board);
+      _changeCurrPlayer();
+    };
+  };
+
+  displayController.cells.forEach((cell) => {
+    cell.addEventListener('click', _clickHandler);
+  });
+})();
